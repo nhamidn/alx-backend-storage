afe3a648a05ecba2eea62c7c198c0d6c9768a8a9
nhamidn <hamid-savana@hotmail.fr>
2023-12-15 00:11:36 +0100

    2-fans.sql

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- Script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+SELECT origin AS country, SUM(fans) AS nb_fans
+FROM metal_bands
+WHERE origin IS NOT NULL
+GROUP BY origin
+ORDER BY nb_fans DESC;

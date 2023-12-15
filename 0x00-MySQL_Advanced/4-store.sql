f8894aff9d336ff079966a508e6f9c4bad3c8b57
nhamidn <nhamid@student.1337.ma>
2023-12-15 16:32:27 +0000

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,4 @@
 -- Script that creates a trigger that decrease a value of item after order.
 CREATE TRIGGER after_order AFTER INSERT on orders FOR EACH ROW
-BEGIN
-	UPDATE items
-	SET quantity = quantity - NEW.number
+	UPDATE items SET quantity = quantity - NEW.number
 	WHERE name = NEW.item_name;
-END

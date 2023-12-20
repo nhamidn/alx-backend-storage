18563b520319b5f442ddd2d0f36354424dac8ba3
nhamidn <nhamid@student.1337.ma>
2023-12-20 21:51:31 +0100

    exercise.py Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,12 +9,27 @@ from typing import Union, Callable, Optional, Any
 
 
 def count_calls(method: Callable) -> Callable:
-    """Function that track the number of redis methods calls"""
+    """Decorator that track the number of redis methods calls"""
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         """Function that increment call count of a method"""
         self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Decorator that stores the calls history of inputs and outputs"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Function that saves the history"""
+        method_name = method.__qualname__
+        input_key = f"{method_name}:inputs"
+        output_key = f"{method_name}:outputs"
+        self._redis.rpush(input_key, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(result))
+        return result
     return wrapper
 
 
@@ -26,6 +41,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes,  int,  float]) -> str:
         """Store data in Redis with a random key and return the key"""

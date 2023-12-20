698dd4297c2cd4a4dc572a9a2f7b41618f75e683
nhamidn <nhamid@student.1337.ma>
2023-12-20 20:23:56 +0100

    exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Exercise module.
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Cache class."""
+    def __init__(self):
+        """Init method of the Cache class."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        rand_key = str(uuid.uuid4())
+        self._redis.set(rand_key, data)
+        return rand_key

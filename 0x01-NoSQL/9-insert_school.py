1d33731cef43eeee70b1755f260581f0f7eedc46
nhamidn <nhamid@student.1337.ma>
2023-12-20 01:10:47 +0100

    9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""9-insert_school module"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Python function that inserts a new document in a collection"""
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id

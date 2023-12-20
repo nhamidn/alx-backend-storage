6b9242af30d2973341c22f121efebd4858c3ee3a
nhamidn <nhamid@student.1337.ma>
2023-12-20 03:47:08 +0100

    102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+12-log_stats module
+"""
+
+from pymongo import MongoClient
+
+
+def show_stats():
+    """
+    Python Function that provides some stats about Nginx logs
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    numGET = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{numGET} status check")
+    print("IPs:")
+    sorted_ips = logs_collection.aggregate(
+        [
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}}
+        ]
+    )
+    for count, value in enumerate(sorted_ips):
+        if count == 10:
+            break
+        print(f"\t{value.get('_id')}: {value.get('count')}")
+
+
+if __name__ == "__main__":
+    show_stats()

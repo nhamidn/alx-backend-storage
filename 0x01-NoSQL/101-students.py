09cde63c46150f375c7e3912a87171e66ad49584
nhamidn <nhamid@student.1337.ma>
2023-12-20 02:50:09 +0100

    101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""101-students module"""
+
+
+def top_students(mongo_collection):
+    """Python function that returns all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {"$project": {
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])

af6275724511ad60b760eea3e23c494dbd461054
nhamidn <nhamid@student.1337.ma>
2023-12-20 20:31:21 +0100

    annotate the __init__ method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,19 +3,19 @@
 Exercise module.
 """
 import redis
-import uuid
+from uuid import uuid4
 from typing import Union
 
 
 class Cache:
     """Cache class."""
-    def __init__(self):
+    def __init__(self) -> None:
         """Init method of the Cache class."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in Redis with a random key and return the key"""
-        rand_key = str(uuid.uuid4())
+        rand_key = str(uuid4())
         self._redis.set(rand_key, data)
         return rand_key

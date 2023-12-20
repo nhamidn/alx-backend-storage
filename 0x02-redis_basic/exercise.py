2dd492e3d9c3c8ccf740cde9208e71aa1a0bf363
nhamidn <nhamid@student.1337.ma>
2023-12-20 20:38:42 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,8 +15,6 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
-    @count_calls
     def store(self, data: Union[str, bytes,  int,  float]) -> str:
         """Store data in Redis with a random key and return the key"""
         key = str(uuid4())

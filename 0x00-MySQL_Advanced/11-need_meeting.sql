398d091d8fb78847417a178a2da1e8e1f138eab0
nhamidn <nhamid@student.1337.ma>
2024-01-01 16:41:16 +0100

    11-need_meeting.sql

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@ CREATE VIEW need_meeting AS
 SELECT name
 FROM students
 WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < CURDATE() - INTERVAL 1 MONTH);
+AND (students.last_meeting IS NULL OR students.last_meeting < CURDATE() - INTERVAL 1 MONTH);
